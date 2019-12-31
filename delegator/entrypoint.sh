365bc91b544db37fc21c4c929187eda262c7cabc
Chris Price <cprice@pingidentity.com>
2019-12-31 15:05:50 -0800

    Initial config with  Delegator v4

---
+++ delegator/entrypoint.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env sh
+#
+# Ping Identity DevOps - Docker Build Hooks
+#
+
+if test "${1}" = "start-server" ; then
+    ${PF_HOST:=localhost}
+    ${PF_PORT:=9031}
+    ${PF_CLIENT_ID:=dadmin}
+
+    echo "
+##################################################################################
+#               Ping Identity DevOps Delegator Web Application
+##################################################################################
+# 
+#     Configured with the following values.  
+# 
+#       PF_HOST: ${PF_HOST}
+#       PF_PORT: ${PF_PORT}
+#       PF_CLIENT_ID: ${PF_CLIENT_ID}
+# 
+#     To set via a docker run or .yaml just set them using examples below
+#
+#    docker run
+#           ...
+#           --env PF_HOST=myhost.mydomain.com
+#           ...
+#
+#      To use with '.yaml' file (use snippet below)
+#
+#    pingdirectry:
+#       environment: PF_HOST=myhost.mydomain.com
+##################################################################################
+"
+
+    cd /usr/share/nginx/html/delegator || echo "Unable to cd to the delegator html directory"
+
+    sed -e "s/PF_HOST = 'localhost'/PF_HOST = '${PF_HOST}'/" \
+        -e "s/PF_PORT = '9031'/PF_PORT = '${PF_PORT}'/" \
+        -e "s/DADMIN_CLIENT_ID = 'dadmin'/DADMIN_CLIENT_ID = '${PF_CLIENT_ID}'/" \
+        "example.config.js" > "config.js"
+
+    chmod 644 "config.js"
+
+    cd /
+    nginx -g 'daemon off;'
+else
+    exec "$@"
+fi
